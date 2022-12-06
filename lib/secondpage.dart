63f9397fdbeca37f0afc5fcf4cb947182d40e6fc
Ashish131294 <ashish.kachhadiya1114@gmail.com>
2022-12-06 09:12:50 +0530

    Initial commit

--- lib/secondpage.dart
+++ lib/secondpage.dart
@@ -73,7 +73,7 @@ class _secondpageState extends State<secondpage> {
               child: ElevatedButton(
                   onPressed: () async {
                     if (play) {
-                      await player.pause();//play
+                      await player.pause();//play paly
                     } else {
                       String path = widget.songs[widget.index].data;
                       await player.play(DeviceFileSource(path));
